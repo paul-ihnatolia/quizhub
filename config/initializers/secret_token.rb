f4e1d99a729d9fedba48428c9f6e940b88657012
Paul Ihnatolia <ihnatolia3@gmail.com>
2012-11-09 11:21:55 +0200

    Created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quizhub::Application.config.secret_token = '0068305c1bbf99f42a84f1f8476415182855a66a934c4e0884376708490a2c1c6a208f3e5d82f1dbfdec83815318aefcdf0d348c7843ac81c9f4d26bd5ca7699'
