dd787ace5772334f53f23b529c522c03643a8159
Paul Ihnatolia <ihnatolia3@gmail.com>
2012-11-13 17:16:48 +0200

    Added for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Quizhub::Application.initialize!
 Encoding.default_internal = Encoding.find("UTF-8")
+ActiveSupport::Deprecation.silenced = true
