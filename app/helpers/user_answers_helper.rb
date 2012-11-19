8288cc4d5c1e96b806f26f01d56fc846e705783d
Paul Ihnatolia <ihnatolia3@gmail.com>
2012-11-19 19:05:41 +0200

    Add success to result

--- app/helpers/user_answers_helper.rb
+++ app/helpers/user_answers_helper.rb
@@ -1,5 +1,12 @@
 module UserAnswersHelper
+
  def getRightAnswerForQuestionId question_id
   Answer.where(question_id: question_id, right: true).first.content
  end
+
+ def percent(right,all)
+   rez = right.to_f/all*100
+   rez.rund
+ end
+
 end
